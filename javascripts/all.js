a8a97741cdf700c302c2c48c13aa4876cb7ed4fd
jody.vanden@gmail.com <jody.vanden@gmail.com>
2017-02-27 15:25:21 +0100

    Automated commit at 2017-02-27 14:25:21 UTC by middleman-deploy 1.0.0

---
+++ javascripts/all.js
@@ -0,0 +1,8 @@
+require jquery
+require bootstrap-sprockets
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+// <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+require_tree
+;
